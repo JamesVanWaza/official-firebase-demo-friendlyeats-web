0c560e6a431a98986d35cd5ade1e6e9f60f07a9c
JamesVanWaza <jgacuca@gmail.com>
2023-08-07 20:30:15 -0400

    Reinitialized Repo

---
+++ tables.php
@@ -0,0 +1,125 @@
+<?php include_once 'nav-admin.php';?>
+	
+<div class="container-fluid">
+		<div class="row">
+			<div class="small-12 medium-12 large-12 columns">
+				<h2 class="text-center">HTML Entities</h2>
+				<table id="punctuation-characters" width="100%">
+					<thead>
+						<tr>
+							<th scope="col">Glyph</th>
+							<th scope="col">Description</th>
+							<th scope="col">Unicode escape</th>
+							<th scope="col">Entity</th>
+							<th scope="col">Mac</th>
+							<th scope="col">Windows</th>
+							<th scope="col">Linux</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<th scope="row">&ldquo;</th>
+							<td>Left double quotation mark</td>
+							<td>\201C</td>
+							<td>&amp;ldquo;</td>
+							<td>Option-[</td>
+							<td>Alt+0147</td>
+							<td>AltGr+V</td>
+						</tr>
+						<tr>
+							<th scope="row">&rdquo;</th>
+							<td>Right double quotation mark</td>
+							<td>\201D</td>
+							<td>&amp;rdquo;</td>
+							<td>Option-Shift-[</td>
+							<td>Alt+0148</td>
+							<td>AltGr+B</td>
+						</tr>
+						<tr>
+							<th scope="row">&lsquo;</th>
+							<td>Left single quotation mark</td>
+							<td>\2018</td>
+							<td>&amp;lsquo;</td>
+							<td>Option-]</td>
+							<td>Alt+0145</td>
+							<td>AltGr+Shift+V</td>
+						</tr>
+						<tr>
+							<th scope="row">&rsquo;</th>
+							<td>Right single quotation mark</td>
+							<td>\2019</td>
+							<td>&amp;rsquo;</td>
+							<td>Option-Shift-]</td>
+							<td>Alt+0146</td>
+							<td>AltGr+Shift+B</td>
+						</tr>
+						<tr>
+							<th scope="row">«</th>
+							<td>Double left-pointing angle quotation mark</td>
+							<td>\00AB</td>
+							<td>&amp;laquo;</td>
+							<td>Option-\</td>
+							<td>Alt+174</td>
+							<td>AltGr+[</td>
+						</tr>
+						<tr>
+							<th scope="row">»</th>
+							<td>Double right-pointing angle quotation mark</td>
+							<td>\00BB</td>
+							<td>&amp;raquo;</td>
+							<td>Option-Shift-\</td>
+							<td>Alt+175</td>
+							<td>AltGr+]</td>
+						</tr>
+						<tr>
+							<th scope="row">‹</th>
+							<td>Single left-pointing angle quotation mark</td>
+							<td>\2039</td>
+							<td>&amp;lsaquo;</td>
+							<td>Option-Shift-3</td>
+							<td>Alt+0139</td>
+							<td>-</td>
+						</tr>
+						<tr>
+							<th scope="row">›</th>
+							<td>Single right-pointing angle quotation mark</td>
+							<td>\203A</td>
+							<td>&amp;rsaquo;</td>
+							<td>Option-Shift-4</td>
+							<td>Alt+0155</td>
+							<td>-</td>
+						</tr>
+						<tr>
+							<th scope="row">&bdquo;</th>
+							<td>Double low-9 quotation mark</td>
+							<td>\201E</td>
+							<td>&amp;bdquo;</td>
+							<td>Option-Shift-W</td>
+							<td>Alt+0132</td>
+							<td>-</td>
+						</tr>
+						<tr>
+							<th scope="row">‚</th>
+							<td>Single low-9 quotation mark</td>
+							<td>\201A</td>
+							<td>&amp;sbquo;</td>
+							<td>-</td>
+							<td>-</td>
+							<td>-</td>
+						</tr>
+						<tr>
+							<th scope="row"></th>
+							<td>Narrow no-break space</td>
+							<td>\202F</td>
+							<td>&amp;#8239;</td>
+							<td>-</td>
+							<td>-</td>
+							<td>-</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+</div>
+
+<?php include 'footer.php';?>
